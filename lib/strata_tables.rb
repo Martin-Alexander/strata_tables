d90bfd59c76600921c9edae67c7a94975c5c1e7e
Martin-Alexander <martingianna@gmail.com>
2025-08-20 06:29:35 -0400

    removed snapshotting feature

--- lib/strata_tables.rb
+++ lib/strata_tables.rb
@@ -4,15 +4,8 @@ require_relative "strata_tables/connection_adapters/schema_statements"
 require_relative "strata_tables/migration/command_recorder"
 require_relative "strata_tables/models/version"
 require_relative "strata_tables/model"
-require_relative "strata_tables/snapshot"
-require_relative "strata_tables/snapshots/builder"
-require_relative "strata_tables/snapshots/display"
 
 module StrataTables
-  def snapshot(ar_class, time)
-    Snapshots::Builder.build(ar_class, time)
-  end
-
   def as_of_scope(time)
     Thread.current[:strata_tables_as_of_time] = time
 
